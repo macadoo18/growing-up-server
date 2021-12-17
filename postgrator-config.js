5a9f3d9529df67371df487d50668c9d2fde57a62
Mac Hale <machaledesign@gmail.com>
2021-12-16 20:37:17 -0700

    creates comment

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,7 +4,9 @@ module.exports = {
     migrationDirectory: 'migrations',
     driver: 'pg',
     connectionString:
-        process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
-    ssl: !!process.env.SSL,
-    validateChecksums: false
+        process.env.NODE_ENV === 'test'
+            ? process.env.TEST_DATABASE_URL
+            : process.env.DATABASE_URL,
+    ssl: !!process.env.SSL
+    //validateChecksums: false
 };
