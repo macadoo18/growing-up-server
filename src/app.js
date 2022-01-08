0d41ada3577a1fe34741a2e8ceae9e8bc8946b9b
Mac Hale <machaledesign@gmail.com>
2022-01-08 12:53:40 -0700

    edits cors 2

--- src/app.js
+++ src/app.js
@@ -25,18 +25,18 @@ app.use(morgan(morganOptions));
 app.use('/uploads', express.static('uploads'));
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.setHeader(
-        'Access-Control-Aloow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS, PUT'
-    );
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     res.setHeader(
+//         'Access-Control-Aloow-Methods',
+//         'GET, POST, PATCH, DELETE, OPTIONS, PUT'
+//     );
+//     next();
+// });
 app.use(helmet());
 
 app.use('/api/auth', authRouter);
