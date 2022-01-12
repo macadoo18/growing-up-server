688a244b5683eb017e3ad0a3cad8632679677193
Mac Hale <machaledesign@gmail.com>
2022-01-12 11:38:43 -0700

    edits cors logic

--- src/app.js
+++ src/app.js
@@ -24,24 +24,7 @@ const origin =
 
 app.use(morgan(morganOptions));
 app.use('/uploads', express.static('uploads'));
-//app.use(cors({ origin }));
-
-app.use((req, res, next) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        'https://growing-up-zeta.vercel.app'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.setHeader(
-        'Access-Control-Aloow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS, PUT'
-    );
-    next();
-});
-
+app.use(cors({ origin }));
 app.use(helmet());
 
 app.use('/api/auth', authRouter);
