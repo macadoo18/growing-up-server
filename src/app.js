1229b085345f97dc6742ecf728bf34cac606c9e8
Mac Hale <machaledesign@gmail.com>
2022-01-12 11:22:20 -0700

    adds image upload back in

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const sleepingRouter = require('./sleeping/sleeping-router');
 const fileUpload = require('express-fileupload');
 
 const app = express();
-//app.use(express.json({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.json());
 app.use(fileUpload());
 
@@ -23,7 +23,7 @@ const origin =
         : 'http://localhost:7000';
 
 app.use(morgan(morganOptions));
-// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 app.use(cors({ origin }));
 app.use(helmet());
 
